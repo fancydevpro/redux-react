ea66ae95322cada8a2f355c74f30422d9b1d3207
fancydevpro <fancydevpro@gmail.com>
2019-01-20 04:04:06 +0200

    provide store

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,6 +1,16 @@
 import { createStore } from 'redux'
 import rootReducer from '../reducers'
+import { addArticle } from '../actions'
 
-const store = createStore(rootReducer)
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+store.subscribe(() => console.log('subscribe callback fired!'))
+store.dispatch(addArticle({
+  title: 'React Redux Tutorial',
+  id: 1
+}))
 
 export default store
