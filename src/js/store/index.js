95eb1765f2ee5776453f68b2ed2cf9340d74272e
fancydevpro <fancydevpro@gmail.com>
2019-01-20 05:58:47 +0200

    add middleware with redux devtools

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,10 +1,13 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from '../reducers'
 import { addArticle } from '../actions'
+import { forbiddenWordsMiddleware } from '../middleware'
+
+const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  storeEnhancers(applyMiddleware(forbiddenWordsMiddleware))
 )
 
 store.subscribe(() => console.log('subscribe callback fired!'))
