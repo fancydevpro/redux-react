b6581da81966bf6596f771a423391e3c68efd269
fancydevpro <fancydevpro@gmail.com>
2019-01-20 06:13:17 +0200

    add Error component

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,4 +1,4 @@
-import { ADD_ARTICLE } from '../constants/action-types'
+import { ADD_ARTICLE, FOUND_BAD_WORD } from '../constants/action-types'
 
 const initialState = {
   articles: []
@@ -8,6 +8,10 @@ function rootReducer(state = initialState, action) {
   if (action.type === ADD_ARTICLE) {
     return Object.assign({}, state, {
       articles: state.articles.concat(action.payload)
+    })
+  } else if (action.type === FOUND_BAD_WORD) {
+    return Object.assign({}, state, {
+      error: true, 
     })
   }
   return state
