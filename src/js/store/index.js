da360ea109971deed01766af4b0146a6179c44f1
fancydevpro <fancydevpro@gmail.com>
2019-01-20 09:16:01 +0200

    implement thunk

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,4 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+
 import rootReducer from '../reducers'
 import { addArticle } from '../actions'
 import { forbiddenWordsMiddleware } from '../middleware'
@@ -7,7 +9,7 @@ const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
   rootReducer,
-  storeEnhancers(applyMiddleware(forbiddenWordsMiddleware))
+  storeEnhancers(applyMiddleware(forbiddenWordsMiddleware, thunk))
 )
 
 store.subscribe(() => console.log('subscribe callback fired!'))
