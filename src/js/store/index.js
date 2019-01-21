1550b92106d7d2e88123a02acce8326037220720
fancydevpro <fancydevpro@gmail.com>
2019-01-21 12:02:01 +0200

    apply saga middleware

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,18 +1,57 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
+import createSagaMiddleware from 'redux-saga'
+import test from 'tape'
+import {
+  put,
+  call,
+} from 'redux-saga/effects'
 
 import rootReducer from '../reducers'
-import { addArticle } from '../actions'
+import { 
+  addArticle, 
+  watchIncrementAsync, 
+  incrementAsync, 
+  rootSaga,
+  delay,
+} from '../actions'
 import { forbiddenWordsMiddleware } from '../middleware'
 
 const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const sagaMiddleware = createSagaMiddleware()
 
 const store = createStore(
   rootReducer,
-  storeEnhancers(applyMiddleware(forbiddenWordsMiddleware, thunk))
+  storeEnhancers(applyMiddleware(forbiddenWordsMiddleware, thunk, sagaMiddleware))
 )
 
-store.subscribe(() => console.log('subscribe callback fired!'))
+sagaMiddleware.run(rootSaga)
+
+test('incrementAsync Saga test', (assert) => {
+  const gen = incrementAsync()
+
+  assert.deepEqual(
+    gen.next().value,
+    call(delay, 1000),
+    'incrementAsync Saga must call delay(1000)'  
+  )
+
+  assert.deepEqual(
+    gen.next().value,
+    put({ type: 'INCREMENT'}),
+    'incrementAsync Saga must dispatch an INCREMENT action'
+  )
+
+  assert.deepEqual(
+    gen.next(),
+    { done: true, value: undefined },
+    'incrementAsync Saga must be done'
+  )
+
+  assert.end()
+})
+
+store.subscribe(() => console.log('subscribe callback fired! ', store.getState()))
 store.dispatch(addArticle({
   title: 'React Redux Tutorial',
   id: 1
